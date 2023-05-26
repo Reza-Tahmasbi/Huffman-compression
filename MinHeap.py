fdd70908c0c5c4b72aacf2672d7c6cb014606792
rezatb <103198863+rezatb@users.noreply.github.com>
2023-05-26 09:12:11 +0330

    switch to python 3rd commit

---
+++ MinHeap.py
@@ -0,0 +1,66 @@
+# A Huffman tree node
+class MinHeapNode:
+    def __init__(self, key, freq):
+        self.key = key
+        self.freq = freq
+        self.left = None
+        self.right = None
+
+    def __lt__(self, other):
+        if self.freq == other.freq:
+            return self.key < other.key
+        return self.freq < other.freq
+
+
+# A class representing a min heap
+class MinHeap:
+    def __init__(self):
+        self.heap = []
+        self.size = 0
+
+    def parent(self, i):
+        return (i - 1) // 2
+
+    def left_child(self, i):
+        return 2 * i + 1
+
+    def right_child(self, i):
+        return 2 * i + 2
+
+    def insert(self, node):
+        self.heap.append(node)
+        self.size += 1
+        i = self.size - 1
+        parent = self.parent(i)
+        while i != 0 and self.heap[parent].freq > self.heap[i].freq:
+            self.heap[i], self.heap[parent] = self.heap[parent], self.heap[i]
+            i = parent
+
+    def extract_min(self):
+        if self.size == 0:
+            return None
+
+        if self.size == 1:
+            self.size -= 1
+            return self.heap.pop()
+
+        root = self.heap[0]
+        self.heap[0] = self.heap.pop()
+        self.size -= 1
+        self.min_heapify(0)
+        return root
+
+    def min_heapify(self, i):
+        smallest = i
+        left = self.left_child(i)
+        right = self.right_child(i)
+
+        if left < self.size and self.heap[left].freq < self.heap[smallest].freq:
+            smallest = left
+
+        if right < self.size and self.heap[right].freq < self.heap[smallest].freq:
+            smallest = right
+
+        if smallest != i:
+            self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
+            self.min_heapify(smallest)
