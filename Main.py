30710893cc06517185d5c83802a2313b12c1654a
Reza <rezatb2002@gmail.com>
2023-06-15 10:56:18 +0330

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,8 +2,8 @@ from Huffman import huffman_encoder_decoder, encode_image
 
 # Driver code
 if __name__ == "__main__":
-    # encoded, decoded, code_map, freq_map, root, isParagraph = huffman_encoder_decoder(input())
-    encoded, decoded, code_map, freq_map, root = encode_image("flower.jpg")
+    encoded, decoded, code_map, freq_map, root, isParagraph = huffman_encoder_decoder(input())
+    # encoded, decoded, code_map, freq_map, root = encode_image("flower.jpg")
     charList = sorted(code_map)
     for key in charList:
         print(key, code_map[key])
